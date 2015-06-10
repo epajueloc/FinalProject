b943315a957e640629d73bfb490c4e0000bfaa39
epajueloc <epajuelo@nd.edu>
2015-06-10 19:15:51 +0200

    Update user signup

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,25 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    	sign_in_and_redirect(@user)
+    else
+    	redirect_to root_path
+    end
+  end
+
+  def update
+    super
+  end
+  def after_sign_up_path_for(resource)
+    '/'
+  end
+  private
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone, :description, :role)
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" } do
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
