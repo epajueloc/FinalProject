5da2f5f86dca22a19ce2aec26887eefa0fdfe271
stanleyzuo <sjrzuo@gmail.com>
2015-06-11 20:27:11 +0200

    added some authorizations

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,12 @@
 class ClientsController < ApplicationController
 	before_action :authenticate_user!
 	def index
-		@categories = Category.all
-		@user = current_user
+
+		if current_user.client?
+				@categories = Category.all
+				@user = current_user
+		elsif current_user.worker?
+			redirect_to :root, alert: "You don't have permission to access this page."
+		end
 	end
 end
--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,20 +1,32 @@
 class WorkersController < ApplicationController
 	  before_action :authenticate_user!
 	def index
-		@categories = Category.all
-		@user = current_user
+		if current_user.worker?
+			@categories = Category.all
+			@user = current_user
+		elsif current_user.client?
+			redirect_to :root, alert: "You don't have permission to access this page."
+		end
 	end
 
 	def show
-		@user = current_user
+			@user = User.find(params[:id])
+			@category = Category.find(params[:category_id])
 	end
 
 	def edit
+		if current_user.worker?
 		@worker = current_user
 		@categories = Category.all
+		elsif current_user.client?
+			redirect_to :root, alert: "You don't have permission to access this page."
+		end
 	end
 
 	def update
+
+		if current_user.worker?
+
 		@worker = current_user
 		@worker.update(worker_params)
 		worker_categories = params[:user][:user][:category_ids]
@@ -25,6 +37,11 @@ class WorkersController < ApplicationController
 		end
 		@worker.save
 		redirect_to worker_path @worker
+
+		elsif current_user.client?
+			redirect_to :root, alert: "You don't have permission to access this page."
+		end
+
 	end
 
 	private
